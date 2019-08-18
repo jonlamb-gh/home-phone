c4594b01bd79bdfdd354de4c01e0cc756108f0e8
Jon Lamb <lamb.jon.io@gmail.com>
2019-08-18 06:59:54 -0700

    LEDs and USART console

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,14 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
                          // extern crate panic_semihosting; // logs messages to the host stderr;
                          // requires a debugger
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
+use nucleo_f767zi::debug_console::DebugConsole;
 use nucleo_f767zi::hal::prelude::*;
+use nucleo_f767zi::hal::serial::Serial;
 use nucleo_f767zi::hal::stm32f7x7;
-use nucleo_f767zi::led::Leds;
+use nucleo_f767zi::hal::timer::Timer;
+use nucleo_f767zi::led::{Color, Leds};
 
 #[entry]
 fn main() -> ! {
@@ -29,6 +33,7 @@ fn main() -> ! {
     let mut rcc = peripherals.RCC.constrain();
 
     let mut gpiob = peripherals.GPIOB.split(&mut rcc.ahb1);
+    let mut gpiod = peripherals.GPIOD.split(&mut rcc.ahb1);
 
     let led_r = gpiob
         .pb14
@@ -40,20 +45,42 @@ fn main() -> ! {
         .pb7
         .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
 
-    // Default clock configuration runs at 16 MHz
-    let _clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let usart3_tx = gpiod.pd8.into_af7(&mut gpiod.moder, &mut gpiod.afrh);
+    let usart3_rx = gpiod.pd9.into_af7(&mut gpiod.moder, &mut gpiod.afrh);
 
-    // TODO
-    // configure maximum clock frequency at 200 MHz
-    //let clocks = rcc.cfgr.freeze_max(&mut flash.acr);
+    // TODO - revist my RCC bits
+    // Default clock configuration runs at 16 MHz
+    //let _clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze_max(&mut flash.acr);
 
     let mut leds = Leds::new(led_r, led_g, led_b);
     for led in leds.iter_mut() {
-        led.on();
-        //led.off();
+        //led.on();
+        led.off();
     }
 
+    // USART3 is routed up to the same USB port as the stlink
+    let serial = Serial::usart3(
+        peripherals.USART3,
+        (usart3_tx, usart3_rx),
+        115_200.bps(),
+        clocks,
+        &mut rcc.apb1,
+    );
+
+    let mut debug_console = DebugConsole::new(serial);
+
+    let mut timer = Timer::tim2(peripherals.TIM2, 1.hz(), clocks, &mut rcc.apb1);
+
+    writeln!(debug_console, "Board initialized").ok();
+
+    leds[Color::Green].toggle();
+
     loop {
-        // your code goes here
+        if timer.wait().is_ok() == true {
+            leds[Color::Blue].toggle();
+            leds[Color::Green].toggle();
+            writeln!(debug_console, "*").ok();
+        }
     }
 }
