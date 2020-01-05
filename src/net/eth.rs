8a2c2289790ceea7b4426b66093ffec0d56c28d9
Jon Lamb <lamb.jon.io@gmail.com>
2020-01-05 07:30:40 -0800

    Undo the workspace

---
+++ src/net/eth.rs
@@ -0,0 +1,8 @@
+//use smoltcp::iface::EthernetInterface;
+//use smoltcp::socket::{SocketHandle, SocketSet, TcpSocket};
+//use smoltcp::time::Instant as SmoltcpInstant;
+//use smoltcp::wire::Ipv4Address;
+
+pub struct Eth {
+    //
+}
